c9305f4a8cbe5fabb649dc3ba128a1ae46903c55
Hirotaka Kawashima <k.hirotaka@me.com>
2022-09-27 22:14:33 +0900

    reformat file with rustfmt

--- src/utils/postgres.rs
+++ src/utils/postgres.rs
@@ -1,15 +1,10 @@
-use tokio_postgres::{NoTls, Error, Client};
-use chrono::{Local, DateTime};
+use chrono::{DateTime, Local};
+use tokio_postgres::{Client, Error, NoTls};
 
-
-pub async fn create_connection(
-    user: &str, password: &str
-) -> Result<Client,Error> {
+pub async fn create_connection(user: &str, password: &str) -> Result<Client, Error> {
     let config = format!("host=localhost user={} password={}", user, password);
 
-    let (client, connection) = tokio_postgres::connect(
-        config.as_str(), NoTls
-    ).await?;
+    let (client, connection) = tokio_postgres::connect(config.as_str(), NoTls).await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -20,37 +15,38 @@ pub async fn create_connection(
     Ok(client)
 }
 
-
 pub async fn create_user(client: &Client, username: &str) -> Result<i32, Error> {
-    let rows = client.query(
-        "SELECT id FROM user_list ORDER BY id DESC",
-        &[]
-    ).await?;
+    let rows = client
+        .query("SELECT id FROM user_list ORDER BY id DESC", &[])
+        .await?;
 
     if rows.len() == 0 {
         let user_id: i32 = 1;
 
-        client.query(
-            "INSERT INTO user_list\
+        client
+            .query(
+                "INSERT INTO user_list\
                 (id,name)\
              VALUES\
                 ($1::INTEGER, $2::VARCHAR)",
-            &[&user_id, &username]
-        ).await?;
+                &[&user_id, &username],
+            )
+            .await?;
 
         Ok(user_id)
-    }
-    else {
+    } else {
         let latest: Option<i32> = rows[0].get(0);
         let next_id: i32 = latest.unwrap() + 1;
 
-        client.query(
-            "INSERT INTO user_list\
+        client
+            .query(
+                "INSERT INTO user_list\
                 (id,name)\
             VALUES\
                 ($1::INTEGER, $2::VARCHAR)",
-            &[&next_id, &username]
-        ).await?;
+                &[&next_id, &username],
+            )
+            .await?;
 
         Ok(next_id)
     }
@@ -60,26 +56,29 @@ pub async fn record(client: &Client, user_id: i32, temperature: f32) -> Result<(
     // log_id (primary key, i64), date(timestamp), user_id(i32), temperature(f32)
     let timestamp: DateTime<Local> = Local::now();
 
-    let rows = client.query(
-        "SELECT log_id FROM temperatures ORDER BY log_id DESC",
-        &[]
-    ).await?;
+    let rows = client
+        .query("SELECT log_id FROM temperatures ORDER BY log_id DESC", &[])
+        .await?;
 
     let log_id: i64 = if rows.len() == 0 {
         1
-    }
-    else {
+    } else {
         let tmp: Option<i64> = rows[0].get(0);
         tmp.unwrap() + 1
     };
 
-    client.query(
-        "INSERT INTO temperatures\
+    client
+        .query(
+            "INSERT INTO temperatures\
             (log_id,user_id,date,temperature)\
         VALUES\
             ($1::BIGINT, $2::INTEGER, $3::TIMESTAMP WITH TIME ZONE,$4::REAL)",
-        &[&log_id, &user_id, &timestamp, &temperature]
-    ).await?;
+            &[&log_id, &user_id, &timestamp, &temperature],
+        )
+        .await?;
+
+    Ok(())
+}
 
     Ok(())
 }
